e13530d2e0fea17c098cf737419235a7a028ae1e
meleadr <meledina.adriano@gmail.com>
2023-07-04 11:51:18 +0200

    model Server

---
+++ src/main/java/com/meleadr/Server/model/Server.java
@@ -0,0 +1,30 @@
+package com.meleadr.Server.model;
+
+import com.meleadr.Server.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "The IP Address cannot be empty or null")
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+
+}
