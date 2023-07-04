837a64013fd2a6f2a5a58f57ff92ab473b797b1c
meleadr <meledina.adriano@gmail.com>
2023-07-04 11:52:37 +0200

    ServerService implement override

---
+++ src/main/java/com/meleadr/Server/service/implementation/ServerServiceImpl.java
@@ -0,0 +1,67 @@
+package com.meleadr.Server.service.implementation;
+
+import com.meleadr.Server.model.Server;
+import com.meleadr.Server.repo.ServerRepo;
+import com.meleadr.Server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
+public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+    @Override
+    public Server create(Server server) {
+        log.info("Creating server: {}", server.getName());
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress inetAddress = InetAddress.getByName(ipAddress);
+        server.setStatus(inetAddress.isReachable(1000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        log.info("Listing servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
+    }
+
+    @Override
+    public Server get(Long id) {
+        log.info("Getting server: {}", id);
+        return serverRepo.findById(id).get();
+    }
+
+    @Override
+    public Server update(Server server) {
+        log.info("Updating server: {}", server.getName());
+        return serverRepo.save(server);
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        log.info("Deleting server: {}", id);
+        serverRepo.deleteById(id);
+        return true;
+    }
+
+    private String setServerImageUrl(){
+        return "https://picsum.photos/200/300";
+    }
+}
