eb9241ef6225ce5dc6abae9f77f897ba05c0bb1b
meleadr <meledina.adriano@gmail.com>
2023-07-05 08:37:49 +0200

    CORS Filter

--- src/main/java/com/meleadr/Server/ServerApplication.java
+++ src/main/java/com/meleadr/Server/ServerApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -23,5 +28,18 @@ public class ServerApplication {
 			serverRepo.save(new Server(null, "192.168.0.3", "Server 3", "16GB", "Dedicated", "https://www.google.com", Status.SERVER_UP));
 			serverRepo.save(new Server(null, "192.168.0.4", "Server 4", "16GB", "Dedicated", "https://www.google.com", Status.SERVER_UP));
 		};
+	 }
+
+	 @Bean
+	public CorsFilter corsFilter() {
+		 UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		 CorsConfiguration corsConfiguration = new CorsConfiguration();
+		 corsConfiguration.setAllowCredentials(true);
+		 corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200"));
+		 corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept","Authorization","Origin, Accept","X-Requested-With","Access-Control-Request-Method","Access-Control-Request-Headers"));
+		 corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
+		 corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+		 urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+		 return new CorsFilter(urlBasedCorsConfigurationSource);
 	 }
 }
